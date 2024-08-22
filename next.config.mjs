fcc487acfd4e52348850913ebfa9815048075ece
MasterofGames77 <mgambardella16@gmail.com>
2024-08-22 13:29:34 -0400

    edits to config.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    images: {
-      domains: ['vgw-splash-page-frontend-71431835113b.herokuapp.com'],
-    },
-    output: 'standalone',
-  };
-  
-  export default nextConfig;  
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'vgw-splash-page-frontend-71431835113b.herokuapp.com',
+        pathname: '/**',
+      },
+    ],
+  },
+  output: 'standalone', // Useful for deploying to Docker or Vercel
+};
+
+export default nextConfig;
+ 
