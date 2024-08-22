31126c45533cc257d1a0b43823656edde15b8954
MasterofGames77 <mgambardella16@gmail.com>
2024-08-22 13:15:22 -0400

    updated config.mjs file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+      domains: ['vgw-splash-page-frontend-71431835113b.herokuapp.com'],
+    },
+    output: 'standalone',
+  };
+  
+  export default nextConfig;  
